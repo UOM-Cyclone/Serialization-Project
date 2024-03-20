5f4afa2144fbd0a90bb8e4a40e53d2abc0689ba6
Thumul <thumul.dasun@gmail.com>
2024-03-20 19:50:21 +0530

    Change the name of ListItem to BuyListItem

--- POS_APP/src/BuyListItem.java
+++ POS_APP/src/BuyListItem.java
@@ -1,12 +1,12 @@
-public class ListItem {
+public class BuyListItem {
     private int itemCode;
     private String itemName;
     private int quantity;
-    private float discount;
-    private float amount;
-    private float unitPrice;
+    private double discount;
+    private double amount;
+    private double unitPrice;
 
-    ListItem(int itemCode, String itemName, float unitPrice, int quantity, float discount, float amount){
+    BuyListItem(int itemCode, String itemName, double unitPrice, int quantity, double discount, double amount){
         this.itemCode = itemCode;
         this.itemName = itemName;
         this.unitPrice = unitPrice;
@@ -18,7 +18,7 @@ public class ListItem {
     public String toString(int index){
         String strOut = String.format("""
                 %d) %3d : %s
-                %.2f %d %.2f %.2f
+                %.2f %d %.2f % %.2f
                 """,index, this.itemCode, this.itemName, this.unitPrice, this.quantity, this.discount, this.amount);
         return strOut;
     }
